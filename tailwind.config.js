024f8aec9656b2feae69f278914793f4a2f3697a
Pim <pim@improvit.nl>
2024-10-13 09:50:02 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './resources/**/*.blade.php',
-    './src/**/*.php',
-    './src/Buttons/*.php',
-    './config/*.php',
-    './resources/js/*.js'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './resources/**/*.blade.php',
+        './src/**/*.php',
+        './src/Buttons/*.php',
+        './config/*.php',
+        './resources/js/*.js'
+    ],
+    theme: {
+        extend: {},
+    },
+    corePlugins: {
+        preflight: false,
+    },
+    plugins: [],
+    prefix: 'tw-editor-',
 }
